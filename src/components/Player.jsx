5eb33ba52693af30bedb76c2f7b10516fc8c241f
Thiha Htet Zaw <thihahtetzaw7991@gmail.com>
2024-10-26 20:25:44 +0630

    create seekbar functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ import { assets, songsData } from "../assets/assets";
 import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
-  const { track, seekBar, seekBg, playStatus, play, pause } =
+  const { track, seekBar, seekBg, playStatus, play, pause, time, previous, next, seekSong } =
     useContext(PlayerContext);
 
   return (
@@ -22,7 +22,7 @@ const Player = () => {
             src={assets.shuffle_icon}
             alt=""
           />
-          <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
+          <img onClick={previous} className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
           {playStatus ? (
             <img
               onClick={pause}
@@ -38,13 +38,14 @@ const Player = () => {
               alt=""
             />
           )}
-          <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
+          <img onClick={next} className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
           <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
         <div className="flex items-center gap-5">
-          <p>1:06</p>
+          <p>{time.currentTime.minute}:{time.currentTime.second}</p>
           <div
             ref={seekBg}
+            onClick={seekSong}
             className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer"
           >
             <hr
@@ -52,7 +53,7 @@ const Player = () => {
               className="h-1 border-none w-0 bg-green-800 rounded-full"
             />
           </div>
-          <p>3:10</p>
+          <p>{time.totalTime.minute}:{time.totalTime.second}</p>
         </div>
       </div>
       <div className="hidden lg:flex items-center gap-2 opacity-75">
